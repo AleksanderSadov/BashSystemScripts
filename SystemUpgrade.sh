9683e01ce3ad7a36a420a3bda567f539113a6a4c
Aleksander Sadov <aleksandersadov95@gmail.com>
2018-11-07 03:59:45 +0700

    Refactor output messages

--- SystemUpgrade.sh
+++ SystemUpgrade.sh
@@ -1,4 +1,24 @@
-#!/bin/bash -x
+#!/bin/bash
 
-sudo apt update && sudo apt upgrade --with-new-pkgs -y && sudo apt autoremove -y
-: "System upgrade completed"
+echo "INFO: Updating packages list"
+sudo apt-get update -qq
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: Unable to update packages list" >&2
+    exit 1
+fi
+
+echo "INFO: Upgrading packages"
+sudo apt-get upgrade --with-new-pkgs -qq
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: Unable to upgrade packages" >&2
+    exit 1
+fi
+
+echo "INFO: Cleaning unneeded packages"
+sudo apt-get autoremove -qq
+if [[ $? -ne 0 ]]; then
+    echo "WARNING: Unable to clean unneeded packages with apt-get autoremove" >&2
+    exit 1
+fi
+
+echo "SUCCESS: System upgrade completed"
