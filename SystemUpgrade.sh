24e1661de84a3a3a2ffdada542cab737414687b1
Aleksander Sadov <aleksandersadov95@gmail.com>
2018-11-13 23:32:00 +0700

    Silent apt-get messages on SystemUpgrade

--- SystemUpgrade.sh
+++ SystemUpgrade.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo "INFO: Updating packages list"
-sudo apt-get update -qq
+sudo apt-get update -qq > /dev/null
 if [[ $? -ne 0 ]]; then
     echo "ERROR: Unable to update packages list" >&2
     exit 1
 fi
 
 echo "INFO: Upgrading packages"
-sudo apt-get upgrade --with-new-pkgs -qq
+sudo apt-get upgrade --with-new-pkgs -qq > /dev/null
 if [[ $? -ne 0 ]]; then
     echo "ERROR: Unable to upgrade packages" >&2
     exit 1
 fi
 
 echo "INFO: Cleaning unneeded packages"
-sudo apt-get autoremove -qq
+sudo apt-get autoremove -qq > /dev/null
 if [[ $? -ne 0 ]]; then
     echo "WARNING: Unable to clean unneeded packages with apt-get autoremove" >&2
     exit 1
